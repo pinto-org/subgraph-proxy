bcca0ee90d32691664608b9dd075b16f86bd39fb
PintoPirate <189064953+PintoPirate@users.noreply.github.com>
2025-05-22 20:01:54 -0400

    Update error code for endpoint errors to 503

--- proxy-api/src/error/endpoint-error.js
+++ proxy-api/src/error/endpoint-error.js
@@ -1,5 +1,5 @@
 class EndpointError extends Error {
-  constructor(message, statusCode = 500) {
+  constructor(message, statusCode = 503) {
     super(message);
     this.statusCode = statusCode;
     this.showMessage = true;
